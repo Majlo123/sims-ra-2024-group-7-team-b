7376d10def3ede310e155bfe0bb8818866b1dce4
Momcilo02 <mocadj2002@gmail.com>
2024-03-10 13:31:10 +0100

    Add Enumeration class
    Added an enumeration class that contains all enumerations

---
+++ Model/Enumeration.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookingApp.Model
+{
+    public class Enumeration
+    {
+        enum AccommodationType
+        {
+            Apartment,
+            House,
+            Cottage
+        }
+    }
+}
