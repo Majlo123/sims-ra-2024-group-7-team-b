f71a37ae6125cdd4505dc064e5f2bb55d7210221
AleksandarSekulic1 <aleksandar.sekulic02.gmail.com>
2024-03-14 03:01:20 +0100

    Add GuideMainView

---
+++ View/Guide/CreateTour.xaml.cs
@@ -0,0 +1,58 @@
+﻿using BookingApp.Repository;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace BookingApp.View.Guide
+{
+    /// <summary>
+    /// Interaction logic for CreateTour.xaml
+    /// </summary>
+    public partial class CreateTour : Window
+    {
+        private readonly TourRepository _tourRepository;
+        //private readonly CheckPoint
+        private readonly TourTimeRepository _tourTimeRepository;
+        private readonly LocationRepository _locationRepository;
+
+        public ObservableCollection<Image> Images { get; set; }
+        public CreateTour()
+        {
+            InitializeComponent();
+        }
+        private void UploadURL(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void CreateTour_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+        private void CancelButton_Click(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+        private void Click_AddCheckPoint(object sender, RoutedEventArgs e)
+        {
+
+        }
+        private void Click_AddTourDate(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+
+    }
+}
