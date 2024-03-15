fb46e6319a44a70ea26d36b7447eb20daa598101
Mihajlo Bogdanovic <bogdanovicmihajlo68@gmail.com>
2024-03-15 01:33:19 +0100

    Update TouristMainWindow

--- View/Tourist/TouristMainView.xaml.cs
+++ View/Tourist/TouristMainView.xaml.cs
@@ -25,78 +25,104 @@ namespace BookingApp.View.Tourist
     {
         public TourDTO SelectedTour { get; set; }
 
-        
-        public ObservableCollection<Tour> Tours { get; set; }
-        public ObservableCollection<Tour> FilteredTours { get; set; }
 
+        public ObservableCollection<TourDTO> Tours { get; set; }
+        public ObservableCollection<TourDTO> FilteredTours { get; set; }
+
+        public ObservableCollection<LocationDTO> Locations { get; set; }
         public TourRepository TourRepository;
+
+        private LocationRepository LocationRepository { get; set; }
 
         public TouristMainView()
         {
+            InitializeComponent();
             DataContext = this;
             TourRepository = new TourRepository();
-            Tours = new ObservableCollection<Tour>(TourRepository.GetAll());
+            Tours = new ObservableCollection<TourDTO>();
+            Locations = new ObservableCollection<LocationDTO>();
+            LocationRepository = new LocationRepository();
+            FilteredTours = new ObservableCollection<TourDTO>();
+            Update();
 
-            FilteredTours = new ObservableCollection<Tour>();
+        }
+        public void InitializeLocation()
+        {
 
-            InitializeComponent();
+            foreach (Location location in LocationRepository.GetAll())
+            {
+                Locations.Add(new LocationDTO(location));
+            }
+
+
+        }
+        public void Update()
+        {
+            Tours.Clear();
+            foreach (Tour tour in TourRepository.GetAll())
+            {
+                InitializeLocation();
+                tour.Location = LocationRepository.Get(tour.Location.Id);
+
+                Tours.Add(new TourDTO(tour));
+            }
+
         }
 
-       
         private void SearchClick(object sender, RoutedEventArgs e)
+        {
+
+            string location = TextBoxLocation.Text.Trim();
+            string duration = TextBoxDuration.Text.Trim();
+            string language = TextBoxLanguage.Text.Trim();
+            int numGuests = 0;
+            int.TryParse(TextBoxNumGuest.Text.Trim(), out numGuests);
+
+
+            FilteredTours.Clear();
+            foreach (var tour in Tours)
             {
-                
-                string location = TextBoxLocation.Text.Trim();
-                string duration = TextBoxDuration.Text.Trim();
-                string language = TextBoxLanguage.Text.Trim();
-                int numGuests = 0;
-                int.TryParse(TextBoxNumGuest.Text.Trim(), out numGuests);
 
-                
-                FilteredTours.Clear();
-                foreach (var tour in Tours)
+                if (!string.IsNullOrEmpty(location) &&
+                    !tour.Location.ToString().ToLower().Contains(location.ToLower()))
                 {
-                    
-                    if (!string.IsNullOrEmpty(location) &&
-                        !tour.Location.ToString().ToLower().Contains(location.ToLower()))
-                    {
-                        continue; 
-                    }
-
-                    
-                    if (!string.IsNullOrEmpty(duration) &&
-                        tour.Duration.ToString() != duration)
-                    {
-                        continue; 
-                    }
-
-                    
-                    if (!string.IsNullOrEmpty(language) &&
-                        !tour.Language.ToString().ToLower().Contains(language.ToLower()))
-                    {
-                        continue; 
-                    }
-
-                    
-                    if (numGuests > 0 && numGuests > tour.MaxGuests)
-                    {
-                        continue; 
-                    }
-
-                    
-                    FilteredTours.Add(tour);
+                    continue;
                 }
 
-               
-                Table.ItemsSource = FilteredTours;
+
+                if (!string.IsNullOrEmpty(duration) &&
+                    tour.Duration.ToString() != duration)
+                {
+                    continue;
+                }
+
+
+                if (!string.IsNullOrEmpty(language) &&
+                    !tour.Language.ToString().ToLower().Contains(language.ToLower()))
+                {
+                    continue;
+                }
+
+
+                if (numGuests > 0 && numGuests > tour.MaxGuests)
+                {
+                    continue;
+                }
+
+
+                FilteredTours.Add(tour);
             }
-       
+
+
+            Table.ItemsSource = FilteredTours;
+        }
+
         private void ReserveClick(object sender, RoutedEventArgs e)
         {
             if (SelectedTour != null)
             {
-                
-                
+
+
             }
             else
             {
@@ -104,6 +130,6 @@ namespace BookingApp.View.Tourist
             }
         }
 
-        
+
     }
 }
